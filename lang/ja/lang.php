c9c9b39e626fbce1442f158e0a94a703055f3f6a
Hideaki SAWADA <chuno@live.jp>
2016-08-06 10:30:04 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['dontlinkback']          = '&lt;doctoc&gt;：<i>%s</i> ページには <i>%s</i> に戻るリンクがありません。';
+$lang['recursionprevented']    = '&lt;doctoc&gt;：<i>%s</i> で無限ループがありました。<i>%s</i> は既にリンクされています。';
