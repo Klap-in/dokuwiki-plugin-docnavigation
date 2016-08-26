4e0b1becb50cf5a71bc853ed53f8a0fd4ab9fcb9
Sam01 <m.sajad079@gmail.com>
2016-08-26 15:45:12 +0200

    translation update

---
+++ lang/fa/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['dontlinkback']          = '&lt;doctoc&gt;: صفحه <i>%s</i> لینک بازگشت ندارد به <i>%s</i>.';
+$lang['recursionprevented']    = '&lt;doctoc&gt;: بی‌نهایت بازگشت شناسایی شده در <i>%s</i>, <i>%s</i> is لینک‌ شده‌های درحال حاضر.';
