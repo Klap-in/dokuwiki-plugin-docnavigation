83196bc3d4fb0ebe40afd65608e727b19c2df843
klapinklapin <klapinklapin@gmail.com>
2016-10-11 22:55:29 +0200

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author klapinklapin <klapinklapin@gmail.com>
+ */
+$lang['dontlinkback']          = '&lt;doctoc&gt;: pagina <i>%s</i> verwijst niet terug naar <i>%s</i>.';
+$lang['recursionprevented']    = '&lt;doctoc&gt;: oneindige doorverwijzing gedetecteerd op <i>%s</i>, er is al een verwijzing naar <i>%s</i>.';
