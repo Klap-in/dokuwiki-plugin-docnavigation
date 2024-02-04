2e6045fb554c6bee47b86ad7e6fb6222ae099159
mod <modex@mail.ru>
2024-02-04 06:55:36 +0100

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author mod <modex@mail.ru>
+ */
+$lang['dontlinkback']          = '&lt;doctoc&gt;: страница <i>%s</i> не имеет обратной ссылки на <i>%s</i>.';
+$lang['recursionprevented']    = '&lt;doctoc&gt;: бесконечная рекурсия, обнаруженная в <i>%s</i>, <i>%s</i> уже связана.';
