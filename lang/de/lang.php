7cade22c744b29aa022441a22e7d1aa8d0a16f1a
Michael Jahn <michael.jahn1990@gmail.com>
2025-11-29 21:25:44 +0100

    Translation update (de)

---
+++ lang/de/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Michael Jahn <michael.jahn1990@gmail.com>
+ */
+$lang['dontlinkback']          = '&lt;doctoc&gt;: Seite <i>%s</i> verlinkt nicht zurück zu <i>%s</i>.';
+$lang['recursionprevented']    = '&lt;doctoc&gt;: Unendliche Rekursion bei <i>%s</i> erkannt, <i>%s</i> ist bereits verknüpft.';
